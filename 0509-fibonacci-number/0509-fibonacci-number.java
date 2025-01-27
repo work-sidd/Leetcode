718a96e580b2e60d240c900e1e5ec3b1c3ba047f
work-sidd <work.siddharth@outlook.com>
2025-01-28 00:07:25 +0530

    Update 0509-fibonacci-number.java

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,14 +1,11 @@
 class Solution {
     public int fib(int n) {
-        if (n == 0) {
-            return 0;
-        }
-        int a = 0, b = 1, sum = 0;
-        for (int i = 1; i < n; i++) {
+        int a = -1, b = 1, sum = 0;
+        for (int i=0;i<=n;i++) {
             sum = a + b;
             a = b;
             b = sum;
         }
-        return b;
+        return sum;
     }
 }
