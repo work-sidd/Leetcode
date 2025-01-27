18f76e12ee3d7663cb94cfd53600b526fb0781e5
work-sidd <work.siddharth@outlook.com>
2025-01-27 22:36:16 +0530

    Update 0125-valid-palindrome.java

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,6 +1,6 @@
 class Solution {
     public boolean isPalindrome(String s) {
-       String modified= s.trim().toLowerCase().replaceAll("[^a-zA-Z0-9]","");
+       String modified= s.trim().toLowerCase().replaceAll("[^a-zA-Z0-9]",""); // you can't use \\W here, since it matches anything except alphabets, digits and _ (underscore), to still use it you can write replaceAll("[_\\W]", ""); now only alphanumeric characters will be left !
        if(modified.isEmpty()) return true;
        int i=0, j=modified.length()-1;
        while(i<j){
